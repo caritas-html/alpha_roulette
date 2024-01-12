d4a74368b8286f533dc2e51169c06e91ed8eefd5
Your Name <leoexori@hotmail.com>
2024-01-12 19:50:08 -0300

    feat: add switch players initial mechanics

--- Player.js
+++ Player.js
@@ -3,9 +3,16 @@ import Game from "./main.js";
 class Player extends Game {
   constructor(difficult, player1, player2 = "PC") {
     super(difficult);
-    this.player1 = player1;
-    this.player2 = player2;
+    this.player1 = {
+        name: player1,
+        life: 100,
+    };
+    this.player2 = {
+        name: player2,
+        life: 100,
+    };
   }
+
 }
 
 export default Player;
--- match.js
+++ match.js
@@ -10,23 +10,50 @@ class Match extends Player {
   constructor(difficult, player1, player2) {
     super(difficult, player1, player2);
 
-    this.playing = player1;
+    this.currentPlayer = true;
+
     this.gameStart();
   }
 
-  play() {
+  introduce() {
     rl.question(
       "Russian roulette. Take turns with the virtual revolver, escalating the stakes with each pull. Will you be the last one standing? Load the chamber, make strategic choices, and let the adrenaline-fueled risk unfold. In 'Russian Roulette Royale', only the bravest prevail! \n type YES to start: ",
       (userInput) => {
-        if (userInput == "YES") {
-          console.log("Let the game begin...");
+        let response = userInput.toUpperCase();
+        if (response == "YES") {
+          console.clear();
+          this.switchPlayerMechanics(this.player1, this.player2);
+        } else {
+          rl.close();
         }
-        rl.close();
       }
     );
   }
 
-  switchPlayer() {}
+  action(player) {
+    console.log(player);
+    // const actionList = {
+    //     shoot:
+    // }
+  }
+
+  switchPlayerMechanics(player1, player2) {
+    console.log(
+      `\n Let the game begin... \n \n \n Difficult: ${this.difficult}, \n Rounds: ${this.gameConfig.rounds}, \n Bullets: ${this.gameConfig.full}, \n Empty Shells: ${this.gameConfig.blank}, \n \n`
+    );
+
+    if (this.currentPlayer) {
+      this.action(player1);
+      this.currentPlayer = !this.currentPlayer;
+    } else {
+      action(player2);
+      this.currentPlayer = !this.currentPlayer;
+    }
+  }
+
+  play() {
+    this.introduce();
+  }
 }
 
 const tstMatch = new Match("easy", "leo");
