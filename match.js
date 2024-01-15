6f2dbc78039033da7f32334c8f11de883fab2e69
Your Name <leoexori@hotmail.com>
2024-01-15 10:52:50 -0300

    feat: complete switch players mech w/ fixed value

--- match.js
+++ match.js
@@ -10,7 +10,7 @@ class Match extends Player {
   constructor(difficult, player1, player2) {
     super(difficult, player1, player2);
 
-    this.currentPlayer = true;
+    this.currentPlayer = true; // if true (P1), else (P2)
 
     this.gameStart();
   }
@@ -22,6 +22,9 @@ class Match extends Player {
         let response = userInput.toUpperCase();
         if (response == "YES") {
           console.clear();
+          console.log(
+            `\n Let the game begin... \n \n \n Difficult: ${this.difficult}, \n Rounds: ${this.gameConfig.rounds}, \n Bullets: ${this.gameConfig.full}, \n Empty Shells: ${this.gameConfig.blank}, \n`
+          );
           this.switchPlayerMechanics(this.player1, this.player2);
         } else {
           rl.close();
@@ -33,45 +36,47 @@ class Match extends Player {
   action(actor, victim) {
     // action shoot
     let damage = 50; // fixed value for now (todo: make it dynamic)
+    let response = "";
+
     function shot(player) {
       if (player.life) {
         player.life -= damage;
-        return `Player ${player} takes ${damage} damage points. Player has ${player.life} life points.`;
+        response = `\n Player ${player.name} takes ${damage} damage points. \n `;
+        console.log(response);
       }
     }
 
     rl.question(
-      `Current Playing: ${actor.name} \n Life Points: ${actor.life} \n Actions: \n shot (), \n self shot (). \n \n Your action:`,
+      `Current Playing: ${actor.name} \n Actions: \n shot (), \n self shot (). \n \n Your action:`,
       (playerInput) => {
         if (
-          playerInput.trim("") == "shot" ||
-          playerInput.trim("") == "shot()"
+          playerInput.trim(" ") == "shot" ||
+          playerInput.trim(" ") == "shot ()"
         ) {
-          victim.life -= damage;
-          console.log({
-            actor: actor.life,
-            victim: victim.life,
-          });
+          shot(victim);
         } else if (
-          playerInput.trim("") == "self shot" ||
-          playerInput.trim("") == "self shot ()"
+          playerInput.trim(" ") == "self shot" ||
+          playerInput.trim(" ") == "self shot ()"
         ) {
-          actor.life -= damage;
+          shot(actor);
         }
+        this.switchPlayerMechanics(this.player1, this.player2);
       }
     );
   }
 
   switchPlayerMechanics(player1, player2) {
-    console.log(
-      `\n Let the game begin... \n \n \n Difficult: ${this.difficult}, \n Rounds: ${this.gameConfig.rounds}, \n Bullets: ${this.gameConfig.full}, \n Empty Shells: ${this.gameConfig.blank}, \n`
-    );
+    let matchStatus = `\n ${player1.name}: ${player1.life} life points. \n ${player2.name}: ${player2.life} life points. \n`;
+
+    console.clear();
 
     if (this.currentPlayer) {
+      console.log(matchStatus);
       this.action(player1, player2);
       this.currentPlayer = !this.currentPlayer;
     } else {
-      action(player2, player1);
+      console.log(matchStatus);
+      this.action(player2, player1);
       this.currentPlayer = !this.currentPlayer;
     }
   }
